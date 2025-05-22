4a4a0eb6a3973282025a17ecceef1fb2ce0ac9c6
Manjeet Rajput <150042556+manjeetrajput141@users.noreply.github.com>
2025-05-22 14:21:08 +0530

    Update RSAEncryption.java

--- DualServerProject/src/main/java/com/algorithm/RSAEncryption.java
+++ DualServerProject/src/main/java/com/algorithm/RSAEncryption.java
@@ -15,7 +15,7 @@ public class RSAEncryption {
 
     //Encrypting data with public key
     public static String encrypt(String data, PublicKey publicKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
         byte[] encryptedData = cipher.doFinal(data.getBytes());  
         return Base64.getEncoder().encodeToString(encryptedData);  // Return encrypted data as Base64 string
@@ -24,7 +24,7 @@ public class RSAEncryption {
     //Decrypting data with private key
     public static String decrypt(String encryptedData, PrivateKey privateKey) throws Exception {
     	
-        Cipher cipher = Cipher.getInstance("RSA");
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
         
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
         

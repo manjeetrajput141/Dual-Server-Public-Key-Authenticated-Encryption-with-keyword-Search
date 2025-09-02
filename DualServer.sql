adcd94ca79974ed2509bbb5f6534382752841960
Manjeet Rajput <manjeetrajput5555@gmail.com>
2025-09-02 15:26:05 +0530

    Adding MYSQL DB Query

--- DualServer.sql
+++ DualServer.sql
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 create database DualServer;
 show tables;
 
@@ -12,28 +11,49 @@ create table testServer(name varchar(50), pass varchar(20), email varchar(30), d
 
 insert into testServer value ("test","test123","test@gmail.com","01012000","male","9876543211","UP","india");
 
-drop table reg;
-select * from assistantServer;
-select * from testServer;
-delete from testServer where name="rohit";
+#Creating Cloud Server DB
+CREATE TABLE `cloudserver` (
+  `dataID` varchar(100) NOT NULL,
+  `dataKey` text,
+  `dataInfo` text,
+  `privateKey` text,
+  `receiverName` varchar(100) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `testServerApproval` int DEFAULT NULL,
+  `ownerName` varchar(30) DEFAULT NULL,
+  `randomUUID` text NOT NULL,
+  PRIMARY KEY (`dataID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+SELECT * FROM dualserver.cloudserver;
 
-SET SQL_SAFE_UPDATES = 0;
+#Creating Assistantserverfile
+CREATE TABLE `assistantserverfile` (
+  `fileId` int NOT NULL AUTO_INCREMENT,
+  `owner` varchar(50) DEFAULT NULL,
+  `receiver` varchar(50) DEFAULT NULL,
+  `filestatus` varchar(100) DEFAULT NULL,
+  `dataId` varchar(100) DEFAULT NULL,
+  `randomUUID` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`fileId`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+#creating testserverrequest
 
-drop table cloudData;
-create table cloudData(dataID INT primary key,dataKey TEXT,dataInfo TEXT,
-privateKey TEXT,receiverName varchar(100),status varchar(50),testServerApproval int);
-select * from cloudData;
-create table reg(name varchar(50), pass varchar(20), email varchar(30), dob varchar(50), gen varchar(50), phone varchar(20), state varchar(20), country VARCHAR(20),status varchar(20),role varchar(10));
+CREATE TABLE `testserverrequest` (
+  `dataID` varchar(100) NOT NULL,
+  `requestReceiver` varchar(50) DEFAULT NULL,
+  `actualReceiver` varchar(50) DEFAULT NULL,
+  `ownerName` varchar(30) DEFAULT NULL,
+  `Request_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `fileStatus` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`dataID`)
+);
+#creating receiverdata
+CREATE TABLE `receiverdata` (
+  `RID` int NOT NULL AUTO_INCREMENT,
+  `receiverName` varchar(50) DEFAULT NULL,
+  `ownerName` varchar(50) DEFAULT NULL,
+  `dataId` varchar(100) DEFAULT NULL,
+  `downloadKey` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`RID`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-delete from cloudData where dataId=2 or dataId=4;
-
-insert into cloudData(dataID,dataKey,privateKey,receiverName,dataInfo,status) value(143,"Mank","pKEy","Varsha","This is My Info","Approval Pending");
-
-create table allfile(fileId INT auto_increment primary key,owner varchar(50),receiver varchar(50),filestatus varchar(100));
-drop table allfile;
-select * from allfile;
-update allfile set filestatus='Approved' where fileId=1;
-drop table testServerRequest;
-create table testServerRequest(dataID int primary key ,publicKey TEXT,receiverName varchar(50),
-status varchar(50),constraint dataID foreign key(dataID) references cloudData(dataID));
->>>>>>> 778187b2324ef8dde086cba36ecdb9c2e95fca36
